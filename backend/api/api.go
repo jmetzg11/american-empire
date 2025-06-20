a1d8d9ac74163608348d0c7636abcdf197816bfd
jmetzg11alpine <jmetzg11@gmail.com>
2025-06-19 22:32:35 -0500

    Work on ContributeEvent endpoint

--- backend/api/api.go
+++ backend/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"good-guys/backend/models"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -83,40 +84,131 @@ func (h *Handler) ContributeEvent(c *gin.Context) {
 		}
 	}
 
-	// if mediaTypes, exists := form.Value["media[][type]"]; exists {
-	// 	for i, mediaType := range mediaTypes {
-	// 		media := models.Media{
-	// 			EventID: event.ID,
-	// 			Type:    mediaType,
-	// 		}
+	fmt.Println(form.Value)
+	if mediaTypes, exists := form.Value["media[][type]"]; exists {
+		for i, mediaType := range mediaTypes {
+			media := models.Media{
+				EventID: event.ID,
+				Type:    mediaType,
+			}
+			fmt.Println(media)
 
-	// 		if mediaType == "photo" {
-	// 			files := form.File[fmt.Sprintf("media[%d][file]", i)]
-	// 			if len(files) > 0 {
-	// 				file := files[0]
-	// 				filename := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
-	// 				filepath := fmt.Sprintf("uploads/%s", filename)
+			if mediaType == "photo" {
+				files := form.File[fmt.Sprintf("media[%d][file]", i)]
+				if len(files) > 0 {
+					file := files[0]
+					filename := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
+					dir := fmt.Sprintf("data/photos/%d", event.ID)
+					os.MkdirAll(dir, 0755)
+					filepath := fmt.Sprintf("data/photos/%d/%s", event.ID, filename)
 
-	// 				if err := c.SaveUploadedFile(file, filepath); err != nil {
-	// 					continue
-	// 				}
-	// 				media.Path = filepath
-	// 			}
-	// 			if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
-	// 				media.Caption = captions[0]
-	// 			}
-	// 		} else if mediaType == "youtube" {
-	// 			if urls := form.Value[fmt.Sprintf("media[%d][url]", i)]; len(urls) > 0 {
-	// 				media.URL = urls[0]
-	// 			}
-	// 			if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
-	// 				media.Caption = captions[0]
-	// 			}
-	// 		}
-
-	// 		h.DB.Create(&media)
-	// 	}
-	// }
+					if err := c.SaveUploadedFile(file, filepath); err != nil {
+						continue
+					}
+					fmt.Println(filepath)
+					media.Path = filepath
+				}
+				if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
+					media.Caption = captions[0]
+				}
+			} else if mediaType == "youtube" {
+				if urls := form.Value[fmt.Sprintf("media[%d][url]", i)]; len(urls) > 0 {
+					media.URL = urls[0]
+				}
+				if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
+					media.Caption = captions[0]
+				}
+			}
+			fmt.Println(media)
+			h.DB.Create(&media)
+		}
+	}
 
 	c.JSON(200, gin.H{"message": "Event contributed successfully"})
 }
+
+// func (h *Handler) ContributeEvent(c *gin.Context) {
+// 	var event models.Event
+
+// 	event.Title = c.PostForm("title")
+// 	event.Country = c.PostForm("country")
+// 	event.Description = c.PostForm("description")
+
+// 	dateStr := c.PostForm("date")
+// 	parsedDate, err := time.Parse("2006-01-02", dateStr)
+// 	if err != nil {
+// 		c.JSON(400, gin.H{"error": "Invalid date format"})
+// 		return
+// 	}
+// 	event.Date = parsedDate
+
+// 	if err := h.DB.Create(&event).Error; err != nil {
+// 		c.JSON(500, gin.H{"error": "Failed to create event"})
+// 		return
+// 	}
+
+// 	form, _ := c.MultipartForm()
+
+// 	for i := 0; ; i++ {
+// 		nameKey := fmt.Sprintf("source[%d][name]", i)
+// 		urlKey := fmt.Sprintf("source[%d][url]", i)
+
+// 		names, nameExists := form.Value[nameKey]
+// 		urls, urlExists := form.Value[urlKey]
+
+// 		if !nameExists || !urlExists {
+// 			break
+// 		}
+
+// 		source := models.Source{
+// 			EventID: event.ID,
+// 			Name:    names[0],
+// 			URL:     urls[0],
+// 		}
+// 		h.DB.Create(&source)
+// 	}
+
+// 	for i := 0; ; i++ {
+// 		typeKey := fmt.Sprintf("media[%d][type]", i)
+// 		mediaTypes, exists := form.Value[typeKey]
+
+// 		if !exists {
+// 			break
+// 		}
+
+// 		media := models.Media{
+// 			EventID: event.ID,
+// 			Type:    mediaTypes[0],
+// 		}
+
+// 		if mediaTypes[0] == "photo" {
+// 			files := form.File[fmt.Sprintf("media[%d][file]", i)]
+// 			if len(files) > 0 {
+// 				file := files[0]
+// 				filename := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
+// 				dir := fmt.Sprintf("data/photos/%d", event.ID)
+// 				os.MkdirAll(dir, 0755)
+// 				filepath := fmt.Sprintf("data/photos/%d/%s", event.ID, filename)
+
+// 				if err := c.SaveUploadedFile(file, filepath); err != nil {
+// 					continue
+// 				}
+// 				media.Path = filepath
+// 			}
+// 			if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
+// 				media.Caption = captions[0]
+// 			}
+// 		} else if mediaTypes[0] == "youtube" {
+// 			if urls := form.Value[fmt.Sprintf("media[%d][url]", i)]; len(urls) > 0 {
+// 				media.URL = urls[0]
+// 			}
+// 			if captions := form.Value[fmt.Sprintf("media[%d][caption]", i)]; len(captions) > 0 {
+// 				media.Caption = captions[0]
+// 			}
+// 		}
+
+// 		h.DB.Create(&media)
+// 	}
+
+// 	c.JSON(200, gin.H{"message": "Event contributed successfully"})
+// }
